1b4509f15f192866036b3090a16ac1fded9d0fee
Fatih Doğaç <fatih.doqc@gmail.com>
2022-01-20 23:04:00 +0300

    burak

---
+++ functions/index.js
@@ -0,0 +1,116 @@
+require("dotenv").config();
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+});
+const db = admin.firestore();
+/*const express = require("express");
+const app = express;
+app.request(express.json());
+const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);*/
+
+exports.getUsersByCollection = functions
+  .region("europe-west1")
+  .https.onCall(async (data, context) => {
+    var documents = [];
+    var temp = [];
+    var collectionRef = db.collection(data.collection);
+    var allDocuments = await collectionRef.get();
+    allDocuments.forEach((doc) => {
+      temp.push(doc);
+    });
+    for (const doc of temp) {
+      const userCred = await admin.auth().getUser(doc.id);
+      const userInfo = doc.data();
+      const user = {
+        ...userCred,
+        ...userInfo,
+      };
+      documents.push(user);
+    }
+    return documents;
+  });
+
+exports.deleteUser = functions
+  .region("europe-west1")
+  .https.onCall(async (data, context) => {
+    await admin.auth().deleteUser(data.uid);
+  });
+
+exports.createUser = functions
+  .region("europe-west1")
+  .https.onCall(async (data, context) => {
+    const user = await admin.auth().createUser({
+      email: data.email,
+      password: data.password,
+      displayName: "employee",
+    });
+    db.collection("employees").doc(user.uid).set({
+      name: data.name,
+      phone: data.phone,
+      address: data.addr,
+      city: data.city,
+      district: data.dist,
+      neighborhood: data.nbhd,
+    });
+  });
+
+exports.getUser = functions
+  .region("europe-west1")
+  .https.onCall(async (data, context) => {
+    const [userCred, userInfo] = await Promise.all([
+      admin.auth().getUser(data.id),
+      db.collection(data.collection).doc(data.id).get(),
+    ]);
+    return {
+      ...userCred,
+      ...userInfo.data(),
+    };
+  });
+
+exports.getUserByEmail = functions
+  .region("europe-west1")
+  .https.onCall(async (data, context) => {
+    return await admin.auth().getUserByEmail(data.email);
+  });
+
+exports.getAllUsers = functions
+  .region("europe-west1")
+  .https.onCall(async (data, context) => {
+    try {
+      const users = [];
+      var res;
+      if (data === "") {
+        res = await admin.auth().listUsers(1000);
+      } else {
+        res = await admin.auth().listUsers(1000, data);
+      }
+      res.users.forEach((record) => {
+        users.push(record.toJSON());
+      });
+      if (res.pageToken) {
+        return users.concat(this.getAllUsers(res.pageToken));
+      } else {
+        return users;
+      }
+    } catch (err) {
+      throw err;
+    }
+  });
+
+exports.updateEmail = functions
+  .region("europe-west1")
+  .https.onCall(async (data, context) => {
+    admin.auth().updateUser(data.uid, {
+      email: data.email,
+    });
+  });
+
+exports.updatePassword = functions
+  .region("europe-west1")
+  .https.onCall(async (data, context) => {
+    admin.auth().updateUser(data.uid, {
+      password: data.password,
+    });
+  });
---
+++ src/views/FirmRegister.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container d-flex flex-column justify-content-center flex-grow-1">
+    <div class="row mt-3 mt-md-0">
+      <div class="d-grid mx-auto col-md-5 rounded border py-3">
+        <RegisterForm header="Firma Kaydı" user_type="firm" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import RegisterForm from "../components/RegisterForm.vue";
+export default {
+  name: "FirmRegister",
+  components: { RegisterForm },
+};
+</script>
+
+<style></style>
