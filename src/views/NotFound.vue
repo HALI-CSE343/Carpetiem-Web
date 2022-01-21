ffb65921322b3211f35e02ece7219583e7ac2607
BurakYildrm <yldrmburak1167@gmail.com>
2022-01-21 03:49:50 +0300

    404 Image Added

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,4 +1,7 @@
 <template>
+  <img
+    src="https://firebasestorage.googleapis.com/v0/b/hali-4cdae.appspot.com/o/PageNotFound.png?alt=media&token=a231c1f9-cb57-4eae-939d-cc6822b7cc38"
+  />
   <h2>404</h2>
   <h3>Page Not Found</h3>
 </template>
@@ -7,4 +10,9 @@
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+img {
+  width: 20%;
+  height: 25%;
+}
+</style>
